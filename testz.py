1c9a1c5a2b6a2232c805f62abbb5945332835886
Dave <davidrivard4@gmail.com>
2020-11-21 21:28:38 -0500

    got the dynamic adding feature working

--- testz.py
+++ testz.py
@@ -4,24 +4,27 @@ book = source.read()
 questions = []
 options = []
 answers = []
-word = str
-wrdCounter = 0
-for i in book:
-    word = str(word) + i
-    if "?" in word:
-        questions.append(word.strip())
-        word = ""
-    elif "\t" in word:
-        if wrdCounter <= 3:
-            options.append(word.strip())
-            wrdCounter += 1
-        else:
-            options.append([word.strip()])
-            wrdCounter = 0
-        word = ""
-    elif "\n" in word:
-        answers.append(word.strip())
-        word = ""
-    elif "":
-        pass
-print(options)
+
+opCount = 0
+opListCount = 0
+content_list = book.split(",")
+for i in content_list:
+    if "\n" in i:
+        questions.append(i.strip())
+    elif "\t" in i:
+        try:
+            if opCount < 3:
+                options[opListCount].append(i.strip())
+                opCount += 1
+        except IndexError:
+            options.append([])
+            if opCount < 3:
+                options[opListCount].append(i.strip())
+                opCount += 1
+    elif "|" in i:
+        answers.append(i.replace("|", "").strip())
+        opListCount += 1
+        opCount = 0
+
+print(f"Questions {questions}\nOptions {options}\nAnswers {answers}")
+source.close()
